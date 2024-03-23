43dd286934f84068acde0dc8c92f4ff9500fbd41
Angelo Soares <angeelosoares@gmail.com>
2024-03-23 02:27:05 -0300

    chore: updated modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { ReportUploadController } from './controllers/report.controller';
-import { CreateReportService } from './services/reports/create-report.service';
-import { CSVReaderImplementationProvider } from './providers/implementations/csv-reader-impl.provider';
+import { ReportModule } from './modules/report.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  providers: [CreateReportService, CSVReaderImplementationProvider],
-  controllers: [ReportUploadController],
+  imports: [ConfigModule.forRoot(), ReportModule],
 })
 export class AppModule {}
---
+++ backend/src/modules/report.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+
+import { ReportUploadController } from '@/controllers/report.controller';
+import { CreateReportService } from '@/services/reports/create-report.service';
+import { CSVReaderImplementationProvider } from '@/providers/implementations/csv-reader-impl.provider';
+import { ReportsRepositoryImplementation } from '@/infra/database/postgres/repositories/implementations/reports.repository.implementation';
+import { ReportDataRepositoryImplementation } from '@/infra/database/postgres/repositories/implementations/report-data.repository.implementation';
+
+@Module({
+  imports: [],
+  providers: [
+    CreateReportService,
+    {
+      provide: 'CSVReader',
+      useClass: CSVReaderImplementationProvider,
+    },
+    {
+      provide: 'ReportsRepository',
+      useClass: ReportsRepositoryImplementation,
+    },
+    {
+      provide: 'ReportDataRepository',
+      useClass: ReportDataRepositoryImplementation,
+    },
+  ],
+  controllers: [ReportUploadController],
+})
+export class ReportModule {}
